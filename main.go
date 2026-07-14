6fa8cd0c37aec0025e6cd2298627f3951e3bcbc1
RishabhWDB <rishabharya.k@gmail.com>
2026-07-14 22:36:47 +0530

    Created main switch case

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -16,13 +18,17 @@ type Task struct {
 
 type Tasks []Task
 
-func AddTask(tasks Tasks, task string) Tasks {
-	return append(tasks, Task{Id: CreateId(tasks), Description: task, Status: "todo", CreatedAt: time.Now(), UpdatedAt: time.Now()})
+func AddTask(tasks Tasks, description string) Tasks {
+	return append(tasks, Task{Id: CreateId(tasks), Description: description, Status: "todo", CreatedAt: time.Now(), UpdatedAt: time.Now()})
 }
 
-func MarkInProgress(tasks Tasks, id int) (Tasks, error) {
+func MarkInProgress(tasks Tasks, id string) (Tasks, error) {
+	n, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, err
+	}
 	for i, t := range tasks {
-		if t.Id == id {
+		if t.Id == n {
 			tasks[i].Status = "in-progress"
 			tasks[i].UpdatedAt = time.Now()
 			return tasks, nil
@@ -31,9 +37,13 @@ func MarkInProgress(tasks Tasks, id int) (Tasks, error) {
 	return tasks, errors.New("id not Found")
 }
 
-func MarkDone(tasks Tasks, id int) (Tasks, error) {
+func MarkDone(tasks Tasks, id string) (Tasks, error) {
+	n, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, err
+	}
 	for i, t := range tasks {
-		if t.Id == id {
+		if t.Id == n {
 			tasks[i].Status = "done"
 			tasks[i].UpdatedAt = time.Now()
 			return tasks, nil
@@ -42,9 +52,13 @@ func MarkDone(tasks Tasks, id int) (Tasks, error) {
 	return tasks, errors.New("id not Found")
 }
 
-func UpdateTask(tasks Tasks, id int, description string) (Tasks, error) {
+func UpdateTask(tasks Tasks, id string, description string) (Tasks, error) {
+	n, err := strconv.Atoi(id)
+	if err != nil {
+		return tasks, errors.New("invalid id type")
+	}
 	for i, t := range tasks {
-		if t.Id == id {
+		if t.Id == n {
 			tasks[i].Description = description
 			tasks[i].UpdatedAt = time.Now()
 			return tasks, nil
@@ -53,9 +67,13 @@ func UpdateTask(tasks Tasks, id int, description string) (Tasks, error) {
 	return tasks, errors.New("id not Found")
 }
 
-func Delete(tasks Tasks, id int) (Tasks, error) {
+func Delete(tasks Tasks, id string) (Tasks, error) {
+	n, err := strconv.Atoi(id)
+	if err != nil {
+		return tasks, errors.New("invalid id type")
+	}
 	for i, t := range tasks {
-		if t.Id == id {
+		if t.Id == n {
 			return append(tasks[:i], tasks[i+1:]...), nil
 		}
 	}
@@ -77,7 +95,7 @@ func FilterTasks(tasks Tasks, status string) {
 }
 
 func CreateId(tasks Tasks) int {
-	uid := 0
+	uid := 1
 	for _, t := range tasks {
 		if t.Id > uid {
 			uid = t.Id + 1
@@ -86,6 +104,68 @@ func CreateId(tasks Tasks) int {
 	return uid
 }
 
-func main() {
+func ReadInput(tasks Tasks) (string, error) {
+	args := os.Args
+	var initCommands []string
+	initCommands = append(initCommands, "add", "update", "delete", "mark-in-progress", "mark-done", "list")
+	if len(args) < 2 {
+		return "", errors.New("invalid arguments")
+	}
+	for _, cmd := range initCommands {
+		if cmd == args[1] {
+			return cmd, nil
+		}
+	}
+	return "", errors.New("command not found")
 
+}
+
+func main() {
+	args := os.Args
+	var tasks Tasks
+	cmd, err := ReadInput(tasks)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		switch cmd {
+		case "add":
+			tasks = AddTask(tasks, args[2])
+			fmt.Println("Task added successfully")
+			fmt.Println(tasks)
+		case "update":
+			tasks, err = UpdateTask(tasks, args[2], args[3])
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("Task updated successfully")
+			fmt.Println(tasks)
+		case "delete":
+			tasks, err = Delete(tasks, args[2])
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("Task deleted successfully!")
+			fmt.Println(tasks)
+		case "mark-in-progress":
+			tasks, err = MarkInProgress(tasks, args[2])
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("Task marked in-progress successfully!")
+			fmt.Println(tasks)
+		case "mark-done":
+			tasks, err = MarkDone(tasks, args[2])
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println("Task marked done successfully!")
+			fmt.Println(tasks)
+		case "list":
+			if len(args) < 3 {
+				FilterTasks(tasks, "")
+			} else {
+				FilterTasks(tasks, args[2])
+			}
+		}
+	}
 }
