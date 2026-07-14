2ffcefae39e89f18155b592c272d5422acc13d56
RishabhWDB <rishabharya.k@gmail.com>
2026-07-14 23:12:33 +0530

    Handled invalid argument length error

--- main.go
+++ main.go
@@ -129,42 +129,68 @@ func main() {
 	} else {
 		switch cmd {
 		case "add":
-			tasks = AddTask(tasks, args[2])
-			fmt.Println("Task added successfully")
-			fmt.Println(tasks)
-		case "update":
-			tasks, err = UpdateTask(tasks, args[2], args[3])
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("Task updated successfully")
-			fmt.Println(tasks)
-		case "delete":
-			tasks, err = Delete(tasks, args[2])
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("Task deleted successfully!")
-			fmt.Println(tasks)
-		case "mark-in-progress":
-			tasks, err = MarkInProgress(tasks, args[2])
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("Task marked in-progress successfully!")
-			fmt.Println(tasks)
-		case "mark-done":
-			tasks, err = MarkDone(tasks, args[2])
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("Task marked done successfully!")
-			fmt.Println(tasks)
-		case "list":
-			if len(args) < 3 {
-				FilterTasks(tasks, "")
+			if len(args) == 3 {
+				tasks = AddTask(tasks, args[2])
+				fmt.Println("Task added successfully")
+				fmt.Println(tasks)
 			} else {
+				fmt.Println("Invalid argument length")
+			}
+		case "update":
+			if len(args) == 4 {
+				tasks, err = UpdateTask(tasks, args[2], args[3])
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println("Task updated successfully")
+					fmt.Println(tasks)
+				}
+			} else {
+				fmt.Println("Invalid argument length")
+			}
+		case "delete":
+			if len(args) == 3 {
+				tasks, err = Delete(tasks, args[2])
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println("Task deleted successfully!")
+					fmt.Println(tasks)
+				}
+			} else {
+				fmt.Println("Invalid argument length")
+			}
+		case "mark-in-progress":
+			if len(args) == 3 {
+				tasks, err = MarkInProgress(tasks, args[2])
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println("Task marked in-progress successfully!")
+					fmt.Println(tasks)
+				}
+			} else {
+				fmt.Println("Invalid argument length")
+			}
+		case "mark-done":
+			if len(args) == 3 {
+				tasks, err = MarkDone(tasks, args[2])
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println("Task marked done successfully!")
+					fmt.Println(tasks)
+				}
+			} else {
+				fmt.Println("Invalid argument length")
+			}
+		case "list":
+			if len(args) == 2 {
+				FilterTasks(tasks, "")
+			} else if len(args) == 3 {
 				FilterTasks(tasks, args[2])
+			} else {
+				fmt.Println("Invalid argument length")
 			}
 		}
 	}
